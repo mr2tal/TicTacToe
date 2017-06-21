5e7419f1652e0d13c04a56f4380a83a108bc8989
mr2 <nielsrb@outlook.dk>
2017-06-21 20:10:58 +0200

    02 - new while loops

--- TEC_Final_Project_NRB/Game.cs
+++ TEC_Final_Project_NRB/Game.cs
@@ -13,9 +13,9 @@ namespace TEC_Final_Project_NRB
             char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
             int player = 0;
             string input;
-            bool win = false;
+            int win = 0;
 
-            while (win == false)
+            while (win == 0)
             {
                 Console.WriteLine("Welcome to Tic tac toe \n");
                 Console.WriteLine("{0} {1} {2}", arr[0], arr[1], arr[2]);
@@ -55,43 +55,73 @@ namespace TEC_Final_Project_NRB
                 {
                     Console.WriteLine("Invalid input, try again");
                 }
+
+                //win conditions
                 if (arr[0] == arr[1] && arr[1] == arr[2])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[3] == arr[4] && arr[4] == arr[5])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[6] == arr[7] && arr[7] == arr[8])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[0] == arr[3] && arr[3] == arr[6])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[1] == arr[4] && arr[4] == arr[7])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[2] == arr[5] && arr[5] == arr[8])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[0] == arr[4] && arr[4] == arr[8])
                 {
-                    win = true;
+                    win = 1;
                 }
                 else if (arr[2] == arr[4] && arr[4] == arr[6])
                 {
-                    win = true;
+                    win = 1;
+                }
+                else if (arr[0] != '0' && arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8')
+                {
+                    win = 2;
                 }
             
 
 
             }
-            Console.WriteLine("you win");
+            if (win == 1)
+            {
+                if (player == 1)
+                {
+                    Console.WriteLine("{0} {1} {2}", arr[0], arr[1], arr[2]);
+                    Console.WriteLine("{0} {1} {2}", arr[3], arr[4], arr[5]);
+                    Console.WriteLine("{0} {1} {2}", arr[6], arr[7], arr[8]);
+                    Console.WriteLine("player 1 wins \n");
+
+                }
+                else if (player == 2)
+                {
+                    Console.WriteLine("{0} {1} {2}", arr[0], arr[1], arr[2]);
+                    Console.WriteLine("{0} {1} {2}", arr[3], arr[4], arr[5]);
+                    Console.WriteLine("{0} {1} {2}", arr[6], arr[7], arr[8]);
+                    Console.WriteLine("Player 2 wins \n");
+                }
+              
+            }
+            else if (win == 2)
+            {
+                Console.WriteLine("Draw");
+            }
+                
+           
 
 
 
@@ -100,7 +130,7 @@ namespace TEC_Final_Project_NRB
 
         public void FourOnARow()
         {
-
+            Console.WriteLine("Test");
         }
     }
 }
--- TEC_Final_Project_NRB/Menu.cs
+++ TEC_Final_Project_NRB/Menu.cs
@@ -12,33 +12,41 @@ namespace TEC_Final_Project_NRB
         string input1;
         public void PrintMenu()
         {
-            Console.WriteLine("Welcome to the game room \n");
-            Console.WriteLine("Choose your game: \n");
-            Console.WriteLine("1. Tic Tac Toe \n");
-            Console.WriteLine("2. 4 on a row \n");
-            Console.WriteLine("3. exit");
-            input1 = Console.ReadLine();
-            MenuChoose(int.Parse(input1));
+            while (true)
+            {
+                Console.WriteLine("____________________________");
+                Console.WriteLine("Welcome to the game room \n");
+                Console.WriteLine("Choose your game: \n");
+                Console.WriteLine("1. Tic Tac Toe \n");
+                Console.WriteLine("2. 4 on a row \n");
+                Console.WriteLine("3. exit");
+           
+                input1 = Console.ReadLine();
+                MenuChoose(int.Parse(input1));
+            }     
+           
         }
         public void MenuChoose(int input)
         {
-            switch (input)
-            {
-                case 1:
-                    g.TicTacToe();
-                    break;
-                case 2:
-                    g.FourOnARow();
-                    break;
-                case 3:
-                    System.Environment.Exit(0);
-                    break;
+       
+                switch (input)
+                {
+                    case 1:
+                        g.TicTacToe();
+                        break;
+                    case 2:
+                        g.FourOnARow();
+                        break;
+                    case 3:
+                        System.Environment.Exit(0);
+                        break;
 
-                default:
-                    Console.WriteLine("Choose a value of 1 2 or 3");
-                    break;
+                    default:
+                        Console.WriteLine("Choose a value of 1 2 or 3");
+                        break;
 
-            }
+                }
         }
+
     }
 }
